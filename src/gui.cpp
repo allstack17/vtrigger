71a389a1bff75cad22678c017e9d128c1d7e7432
allstack17 <102427851+allstack17@users.noreply.github.com>
2024-11-30 20:49:56 +0300

    opti window render (30 fps) & rm unused includes

--- src/gui.cpp
+++ src/gui.cpp
@@ -6,10 +6,8 @@
  */
 
 #include <iostream>
-#include <windows.h>
-#include <string>
 #include <thread>
-#include <sstream>
+#include <windows.h>
 
 #include "gui.h"
 #include "triggerbot.h"
@@ -27,7 +25,8 @@ static bool GLFWInit();
 static void Render(GLFWwindow*, ConfigFileData&);
 
 /* GuiMain */
-bool GuiMain(ConfigFileData& cfg) {
+bool GuiMain(ConfigFileData& cfg) 
+{
 	if (!GLFWInit())
 		return false;
 
@@ -42,15 +41,17 @@ bool GuiMain(ConfigFileData& cfg) {
 	return true;
 }
 
-#define INPUT_FIELD(a, b, var, limit, flag) {																			\
-	old = var;																											\
-	flag ? ImGui::InputScalar(a, ImGuiDataType_U8, &var, nullptr, nullptr, nullptr, ImGuiInputTextFlags_CharsDecimal)	\
-		 : ImGui::InputInt(a, (int*)&var, 0, 0, ImGuiInputTextFlags_CharsDecimal);										\
-	if (var < 0 || var > limit) var = old;																				\
-	else if (old != var) TRIGGER_MAIN_LOOP = false;																		\
+#define INPUT_FIELD(a, b, var, limit, flag) {\
+	ImGui::SetNextItemWidth(100);\
+	old = var;\
+	flag ? ImGui::InputScalar(a, ImGuiDataType_U8, &var, nullptr, nullptr, nullptr, ImGuiInputTextFlags_CharsDecimal)\
+		 : ImGui::InputInt(a, (int*)&var, 0, 0, ImGuiInputTextFlags_CharsDecimal);\
+	if (var < 0 || var > limit) var = old;\
+	else if (old != var) TRIGGER_MAIN_LOOP = false;\
 }
 
-void Render(GLFWwindow* pwnd, ConfigFileData& cfg) {
+void Render(GLFWwindow* pwnd, ConfigFileData& cfg) 
+{
 	/* imgui init */
 	IMGUI_CHECKVERSION();
 
@@ -66,6 +67,7 @@ void Render(GLFWwindow* pwnd, ConfigFileData& cfg) {
 	ShowWindow(GetConsoleWindow(), SW_HIDE);
 
 	/* 
+	 * keyboard hooker:
 	 * 'VK_INSERT' to display/hide window
 	 * 'VK_END' to switch TRIGGER_MAIN_LOOP
 	 */
@@ -88,55 +90,45 @@ void Render(GLFWwindow* pwnd, ConfigFileData& cfg) {
 		}, nullptr, 0
 	);
 
-	/* avoid glfw window resize */
-	int height;
 	/* protect fields from invalid data */
 	int old;
+	/* windows fps lock (30) */
+	int fps = 0;
 
 	/* loop begin */
-	while (!glfwWindowShouldClose(pwnd)) {
+	while (!glfwWindowShouldClose(pwnd)) 
+	{
 		glfwPollEvents();
 
-		glfwGetWindowSize(pwnd, nullptr, &height);
+		/* framerate */
+		if (fps == 30) {
+			fps = 0;
+			if (global::render_window) {
+				glfwSetWindowSize(pwnd, WINDOW_X_SIZE, WINDOW_Y_SIZE);
+				ImGui_ImplOpenGL3_NewFrame();
+				ImGui_ImplGlfw_NewFrame();
+				ImGui::NewFrame();
 
-		if (global::render_window) {
-			ImGui_ImplOpenGL3_NewFrame();
-			ImGui_ImplGlfw_NewFrame();
-			ImGui::NewFrame();
+				/* imgui begin */
+				ImGui::Begin("Trigger", nullptr,
+					ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
 
-			/* imgui begin */
-			ImGui::Begin("Trigger", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize);
+				/* set imgui window params once */
+				ImGui::SetWindowSize({ WINDOW_X_SIZE, WINDOW_Y_SIZE }, ImGuiCond_Once);
+				ImGui::SetWindowPos({ 0, 0 }, ImGuiCond_Once);
+				ImGui::SetWindowCollapsed(false, ImGuiCond_Once);
 
-			/* set imgui window params once */
-			ImGui::SetWindowSize(
-				{ WINDOW_X_SIZE, WINDOW_Y_SIZE }, ImGuiCond_Once);
-			ImGui::SetWindowPos({ 0, 0 }, ImGuiCond_Once);
-			ImGui::SetWindowCollapsed(false, ImGuiCond_Once);
-
-			/* widgets */
-			if (!ImGui::IsWindowCollapsed()) {
+				/* widgets */
 				ImGui::PushStyleColor(ImGuiCol_Text, { 0.7f, 0.9f, 1.0f, 0.5f });
 				ImGui::SetCursorPosX(WINDOW_X_SIZE / 2 - (ImGui::CalcTextSize("C O N F I G").x / 2));
 				ImGui::Text("C O N F I G");
 				ImGui::PopStyleColor();
 
-				ImGui::SetNextItemWidth(100);
-				INPUT_FIELD(" X (200)", "zone_x", cfg._zone_x, 200, true);
-
-				ImGui::SameLine();
-				ImGui::SetNextItemWidth(100);
+				INPUT_FIELD(" X (200)", "zone_x", cfg._zone_x, 200, true); ImGui::SameLine();
 				INPUT_FIELD(" Y (200)", "zone_y", cfg._zone_y, 200, true);
-
-				ImGui::SetNextItemWidth(100);
 				INPUT_FIELD(" Tolerance (100)", "tolerance", cfg._tolerance, 100, true);
-
-				ImGui::SetNextItemWidth(100);
 				INPUT_FIELD(" Delay (1000)", "delay", cfg._delay, 1000, false);
-
-				ImGui::SetNextItemWidth(100);
 				INPUT_FIELD(" Hotkey (default 'LAlt')", "hotkey", cfg._hotkey, 255, true);
-
-				ImGui::SetNextItemWidth(100);
 				INPUT_FIELD(" Second key (default 'K')", "second_key", cfg._second_key, 255, true);
 
 				ImGui::SetCursorPosX(WINDOW_X_SIZE / 2 - (ImGui::CalcTextSize("'INSERT' show/hide :: 'END' start/stop").x / 2));
@@ -158,48 +150,43 @@ void Render(GLFWwindow* pwnd, ConfigFileData& cfg) {
 				if (ImGui::Button(TRIGGER_MAIN_LOOP ? "STOP" : "START", { 220, 30 }))
 					TRIGGER_MAIN_LOOP = TRIGGER_MAIN_LOOP ? false : true;
 				ImGui::PopStyleColor();
+				/* widgets end */
+
+				/* imgui end */
+				ImGui::End();
+
+				/* render */
+				ImGui::Render();
+				glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+				glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+				ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+				glfwSwapBuffers(pwnd);
 			}
-
-			/* no glfw window (auto resize to imgui) */
-			if (height != (int)ImGui::GetWindowHeight()) {
-				height = (int)ImGui::GetWindowHeight();
-				glfwSetWindowSize(pwnd, WINDOW_X_SIZE, height);
-			}
-
-			/* imgui end */
-			ImGui::End();
-
-			/* render */
-			ImGui::Render();
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-			glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-			glfwSwapBuffers(pwnd);
-		}
-
-		/* ignore loop 'INSERT' */
-		else {
-			if (height)
+			/* ignore loop 'INSERT' */
+			else
 				glfwSetWindowSize(pwnd, 0, 0);
-			__sleep(5);
-		}
 
+			__sleep(33);
+		}
+		++fps;
 	}
 	/* loop end */
 
+	/* cleanup */
 	ImGui_ImplOpenGL3_Shutdown();
 	ImGui_ImplGlfw_Shutdown();
 	ImGui::DestroyContext();
 }
 
-bool GLFWInit() {
+bool GLFWInit() 
+{
 	if (!glfwInit())
 		return false;
 
 	/* glfw window flags */
-	glfwWindowHint(GLFW_DECORATED,				 0);
-	glfwWindowHint(GLFW_RESIZABLE,				 0);
-	glfwWindowHint(GLFW_FLOATING,				 1);
+	glfwWindowHint(GLFW_DECORATED,               0);
+	glfwWindowHint(GLFW_RESIZABLE,               0);
+	glfwWindowHint(GLFW_FLOATING,                1);
 	glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, 1);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,   4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,   5);
